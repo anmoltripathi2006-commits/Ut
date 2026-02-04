0daf8bb955af1d2c092c1d751ab7eeda7f0cdecb
anmoltripathi2006-commits <anmoltripathi2006@gmail.com>
2026-02-05 02:48:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-// Initialize EmailJS
 (function () {
   emailjs.init("HK85t7m_nVNHmHelQ"); // PUBLIC KEY
 })();
 
-// Contact form submit
-document.getElementById("contact-form").addEventListener("submit", function (e) {
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const form = this;
-
-  emailjs.send("service_xgul5s7", "template_whk7z0s", {
-    name: form.name.value,
-    email: form.email.value,
-    message: form.message.value,
-  })
+  emailjs.sendForm(
+    "service_xgul5s7",
+    "template_whk7z0s",
+    form
+  )
   .then(() => {
     alert("✅ Message sent successfully!");
     form.reset();
   })
   .catch((error) => {
+    alert("❌ Failed to send message. Check console.");
     console.error("EmailJS Error:", error);
-    alert("❌ Failed to send message. Please try again.");
   });
 });
